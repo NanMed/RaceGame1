8fa3933bb34a96fcc5087a0cf66d4ad6fc046fa6
JimeBB <jimeberbau@outlook.es>
2019-04-03 16:56:26 -0600

    Entrega2
    cambios entrega 2

---
+++ Assets/Scripts/CarSteer.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSteer : MonoBehaviour {
+
+	[SerializeField]	
+	public Transform[] path;
+	//private List<Transform> nodes;
+	[SerializeField]	
+	private float speed = 2f;
+
+	private int currentNode = 0;
+	//public float maxSteerAngle = 45f;
+	//public WheelCollider wheels;
+
+	// Use this for initialization
+	void Start () {
+
+	transform.position = path[currentNode].transform.position;
+
+
+
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+	}
+
+	void Move(){
+		transform.position = Vector2.MoveTowards(transform.position, path[currentNode].transform.position, speed * Time.deltaTime);
+
+		if(transform.position == path[currentNode].transform.position){
+			currentNode += 1;
+		}else if(currentNode == path.Length)
+			currentNode = 0;
+		
+		}
+	}
+
+/*void ApplySteer(){
+		Vector3 relativeVector = transform.InverseTransformPoint(nodes[currentNode].position);
+	Debug.Log(relativeVector);
+	float newSteer = (relativeVector.x / relativeVector.magnitude) * maxSteerAngle;
+	wheels.steerAngle = newSteer;
+	}
+
+	void Drive(){
+		wheels.motorTorque = 100f;
+		
+	}*/	
+
+/*Transform[] pathTransforms = path.GetComponentsInChildren<Transform>();
+	nodes = new List<Transform>();
+
+	for(int i = 0; i < pathTransforms.Length; i++){
+		if(pathTransforms[i] != path.transform){
+			nodes.Add(pathTransforms[i]);
+		}
+	}*/
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Timer : MonoBehaviour {
+	
+     Image timerBar;
+     public float maxTime = 7.0f;
+     public float timeLeft;
+     public GameObject timesUpText;
+
+     void Start()
+     {
+         timeLeft = maxTime;
+         timerBar = GetComponent<Image>();
+	 timesUpText.SetActive(false);
+     }
+
+	void Update(){
+		if(timeLeft > 0){
+			timeLeft -= Time.deltaTime;
+			timerBar.fillAmount = timeLeft / maxTime;
+		}else{
+			timesUpText.SetActive(true);
+			Time.timeScale = 0;
+		}
+	}
+ 
+}
