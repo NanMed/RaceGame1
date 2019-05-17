c7c54a7d33056c4c2edb1c760d96f4282a675995
JimeBB <jimeberbau@outlook.es>
2019-05-16 21:16:15 -0500

    txtControl

---
+++ Assets/Scripts/txtControl.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class txtControl : MonoBehaviour
+{
+
+    public Text player;
+    public Text coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("counter1") > 1)
+        {
+            player.text = "Congratulations Player 1";
+            coins.text = "Total coins: " + PlayerPrefs.GetInt("coins1");
+        }
+        else
+        {
+            player.text = "Congratulations Player 2";
+            coins.text = "Total coins: " + PlayerPrefs.GetInt("coins2");
+        }
+    }
+
+
+}
